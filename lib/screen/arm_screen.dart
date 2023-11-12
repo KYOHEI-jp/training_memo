f28fad38b02c9086e5a8f8aa03795e8b4cc855b9
KYOHEI-jp <kingdomhearts510@gmail.com>
2023-11-12 23:56:52 +0900

    armscreen

---
+++ lib/screen/arm_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ArmScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("腕のトレーニング"),
+      ),
+      body: Center(
+        child: Text("腕のトレーニング内容"),
+      ),
+    );
+  }
+}

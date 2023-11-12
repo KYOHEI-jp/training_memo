f10ce6d18a515b20cd2f08f62a659ccf4bfb8056
KYOHEI-jp <kingdomhearts510@gmail.com>
2023-11-13 00:15:08 +0900

    脚

---
+++ lib/screen/leg_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class legScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("脚のトレーニング"),
+      ),
+      body: Center(
+        child: Text("脚のトレーニング内容"),
+      ),
+    );
+  }
+}

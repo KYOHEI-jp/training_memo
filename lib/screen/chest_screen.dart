720b712f9a2c5cc2bbbdc4f97121e759b7d7dcb1
KYOHEI-jp <kingdomhearts510@gmail.com>
2023-11-13 00:15:05 +0900

    胸

---
+++ lib/screen/chest_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChestScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("胸のトレーニング"),
+      ),
+      body: Center(
+        child: Text("胸のトレーニング内容"),
+      ),
+    );
+  }
+}

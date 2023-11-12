181b5d8afc00b6f099e5f14345b82c985f8d86e1
KYOHEI-jp <kingdomhearts510@gmail.com>
2023-11-12 23:56:44 +0900

    database

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -1,50 +1,59 @@
-import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'training_record.dart';
 
 class DatabaseHelper {
-  static final _databaseName = "MyDatabase.db";
-  static final _databaseVersion = 1;
-
-  static final table = 'my_table';
-
-  static final columnId = 'id';
-  static final columnName = 'name';
-
-  DatabaseHelper._privateConstructor();
-  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._init();
 
   static Database? _database;
+
+  DatabaseHelper._init();
+
   Future<Database> get database async {
     if (_database != null) return _database!;
-    _database = await _initDatabase();
+
+    _database = await _initDB('training.db');
     return _database!;
   }
 
-  _initDatabase() async {
-    String path = join(await getDatabasesPath(), _databaseName);
-    return await openDatabase(path,
-        version: _databaseVersion,
-        onCreate: _onCreate);
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(path, version: 1, onCreate: _createDB);
   }
 
-  Future _onCreate(Database db, int version) async {
+  Future _createDB(Database db, int version) async {
+    const idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    const textType = 'TEXT NOT NULL';
+    const integerType = 'INTEGER NOT NULL';
+
     await db.execute('''
-          CREATE TABLE $table (
-            $columnId INTEGER PRIMARY KEY,
-            $columnName TEXT NOT NULL
-          )
-          ''');
+CREATE TABLE training_records ( 
+  id $idType, 
+  part $textType,
+  exercise $textType,
+  weight $integerType,
+  reps $integerType
+  )
+''');
   }
 
-  Future<int> insert(Map<String, dynamic> row) async {
-    Database db = await instance.database;
-    return await db.insert(table, row);
+  Future<int> addTrainingRecord(TrainingRecord record) async {
+    final db = await instance.database;
+    return db.insert('training_records', record.toMap());
   }
 
-  Future<List<Map<String, dynamic>>> queryAllRows() async {
-    Database db = await instance.database;
-    return await db.query(table);
+  Future<List<TrainingRecord>> getTrainingRecords() async {
+    final db = await instance.database;
+    const orderBy = 'part ASC';
+    final result = await db.query('training_records', orderBy: orderBy);
+
+    return result.map((json) => TrainingRecord.fromMap(json)).toList();
   }
 
-// その他の必要なCRUD操作をここに追加
+  Future close() async {
+    final db = await instance.database;
+    db.close();
+  }
 }

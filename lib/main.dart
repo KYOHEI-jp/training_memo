1daa628fbd80535cf3c5d6f1d2e9156f54b1cbdb
KYOHEI-jp <kingdomhearts510@gmail.com>
2023-11-13 00:14:47 +0900

    parstscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:training_memo/screen/parts_screen.dart';
-import 'database/counter_model.dart';
-import 'database/database_helper.dart';
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => CounterModel(),
-      child: MyApp(),
-    ),
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Provider and SQLite Example',
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Provider and SQLite Example'),
+      title: 'トレーニングアプリ',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        child: Consumer<CounterModel>(
-          builder: (context, counter, child) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  '${counter.count}',
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-                ElevatedButton(
-                  child: Text('Get All Counts'),
-                  onPressed: () async {
-                    List<Map<String, dynamic>> allCounts =
-                        await counter.getAllCounts();
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: Text("All Counts"),
-                          content: SingleChildScrollView(
-                            child: ListBody(
-                              children: allCounts
-                                  .map((count) => Text(
-                                      count[DatabaseHelper.columnName]
-                                          .toString()))
-                                  .toList(),
-                            ),
-                          ),
-                          actions: <Widget>[
-                            TextButton(
-                              child: Text('Close'),
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  },
-                ),
-              ],
-            );
-          },
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // ここでPartsScreenにナビゲートします
-          Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) => PartsScreen()),
-          );
-        },
-        tooltip: 'Go to Parts Screen',
-        child: Icon(Icons.add),
-      ),
+      home: PartsScreen(), // PartsScreenを直接表示
     );
   }
 }

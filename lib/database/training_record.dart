970a88cefa260bcdf9fa57e78a0fb466a15f582e
KYOHEI-jp <kingdomhearts510@gmail.com>
2023-11-13 00:14:57 +0900

    trainingrecord

--- lib/database/training_record.dart
+++ lib/database/training_record.dart
@@ -1,16 +1,31 @@
 class TrainingRecord {
-  final int? id;
-  final String part;
-  final String exercise;
-  final int weight;
-  final int reps;
+  int? id;       // データベースのID（自動生成されます）
+  String part;   // トレーニングの部位
+  String? exercise; // トレーニングの種目
+  int? weight;    // 使用重量
+  int? reps;      // 実施回数
 
-  TrainingRecord({this.id, required this.part, required this.exercise, required this.weight, required this.reps});
+  TrainingRecord({
+    this.id,
+    required this.part,
+    required this.exercise,
+    required this.weight,
+    required this.reps,
+  });
 
-  // TrainingRecord オブジェクトをマップに変換するメソッド
+  factory TrainingRecord.fromMap(Map<String, dynamic> map) {
+    return TrainingRecord(
+      id: map['id'],
+      part: map['part'],
+      exercise: map['exercise'],
+      weight: map['weight'],
+      reps: map['reps'],
+    );
+  }
+
   Map<String, dynamic> toMap() {
     return {
-      'id': id,         // idは自動生成されるので、nullでも大丈夫です
+      'id': id,
       'part': part,
       'exercise': exercise,
       'weight': weight,

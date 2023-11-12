82b4a4e5f81fb923f1b25986860877ab06cc5bca
KYOHEI-jp <kingdomhearts510@gmail.com>
2023-11-12 23:10:40 +0900

    とりあえずコミット

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'database/counter_model.dart';
+import 'database/database_helper.dart';
 
 void main() {
   runApp(
@@ -14,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Provider and SQLite Example',
       home: HomeScreen(),
     );
   }
@@ -25,34 +27,61 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Provider Example'),
+        title: Text('Provider and SQLite Example'),
       ),
       body: Center(
         child: Consumer<CounterModel>(
           builder: (context, counter, child) {
-            return Text(
-              '${counter.count}',
-              style: Theme.of(context).textTheme.headline4,
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  '${counter.count}',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+                ElevatedButton(
+                  child: Text('Get All Counts'),
+                  onPressed: () async {
+                    List<Map<String, dynamic>> allCounts =
+                        await counter.getAllCounts();
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text("All Counts"),
+                          content: SingleChildScrollView(
+                            child: ListBody(
+                              children: allCounts
+                                  .map((count) => Text(
+                                      count[DatabaseHelper.columnName]
+                                          .toString()))
+                                  .toList(),
+                            ),
+                          ),
+                          actions: <Widget>[
+                            TextButton(
+                              child: Text('Close'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                ),
+              ],
             );
           },
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => Provider.of<CounterModel>(context, listen: false).increment(),
+        onPressed: () =>
+            Provider.of<CounterModel>(context, listen: false).increment(),
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
     );
-  }
-}
-
-class CounterModel with ChangeNotifier {
-  int _count = 0;
-
-  int get count => _count;
-
-  void increment() {
-    _count++;
-    notifyListeners(); // リスナーに変更を通知
   }
 }

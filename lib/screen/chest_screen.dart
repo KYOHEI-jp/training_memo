85c2dd8d6e8861d3e50be9952954a3e1ad48e7b1
KYOHEI-jp <kingdomhearts510@gmail.com>
2023-11-18 21:05:07 +0900

    胸

--- lib/screen/chest_screen.dart
+++ lib/screen/chest_screen.dart
@@ -1,14 +1,73 @@
 import 'package:flutter/material.dart';
+import '../database/training_record.dart';
+import 'weight_screen.dart';
 
 class ChestScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    List<String> exercises = [
+      'ベンチプレス',
+      'ダンベルフライ',
+      'プッシュアップ',
+      'ペックデックマシン',
+      'インクラインベンチプレス',
+      'ディップス',
+      'ケーブルクロスオーバー',
+      'デクラインベンチプレス',
+      'スミスマシンベンチプレス',
+      'チェストプレスマシン',
+      'フロアプレス',
+      'バタフライ',
+      'プルオーバー',
+      'スタンディングチェストプレス',
+      'ワンアームダンベルベンチプレス'
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Text("胸のトレーニング"),
       ),
-      body: Center(
-        child: Text("胸のトレーニング内容"),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        padding: EdgeInsets.all(10),
+        itemCount: exercises.length,
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () {
+              TrainingRecord record = TrainingRecord(
+                part: '胸',
+                exercise: exercises[index],
+                weight: null,
+                reps: null,
+              );
+
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => WeightScreen(record: record),
+                ),
+              );
+            },
+            child: Container(
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Text(
+                exercises[index],
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          );
+        },
       ),
     );
   }

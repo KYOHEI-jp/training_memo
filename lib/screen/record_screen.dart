c475cae1bf5092e6edb95067e9545dc7a9916a52
KYOHEI-jp <kingdomhearts510@gmail.com>
2023-11-19 19:27:20 +0900

    RecordScreenの遷移変更

--- lib/screen/record_screen.dart
+++ lib/screen/record_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart'; // DateFormatのために必要
-import 'package:training_memo/database/database_helper.dart';
+import 'package:intl/intl.dart';
 import '../database/training_record.dart';
-import 'parts_screen.dart'; // PartsScreenをインポート
+import 'app_screen.dart';
+import 'parts_screen.dart';
+import 'package:training_memo/database/database_helper.dart';
 
 class RecordScreen extends StatefulWidget {
   @override
@@ -23,38 +24,49 @@ class _RecordScreenState extends State<RecordScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text("トレーニング記録"),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.of(context).pushReplacement(
-              MaterialPageRoute(builder: (context) => PartsScreen()),
-            );
-          },
-        ),
       ),
-      body: FutureBuilder<List<TrainingRecord>>(
-        future: records,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                final record = snapshot.data![index];
-                String formattedDate = DateFormat('yyyy/MM/dd HH:mm').format(record.createdAt ?? DateTime.now());
-                return Card(
-                  child: ListTile(
-                    title: Text('${record.part} - ${record.exercise}'),
-                    subtitle: Text('重量: ${record.weight} kg, 回数: ${record.reps}回, 日時: $formattedDate'),
-                  ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: FutureBuilder<List<TrainingRecord>>(
+              future: records,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      final record = snapshot.data![index];
+                      String formattedDate = DateFormat('yyyy/MM/dd HH:mm')
+                          .format(record.createdAt ?? DateTime.now());
+                      return Card(
+                        child: ListTile(
+                          title: Text('${record.part} - ${record.exercise}'),
+                          subtitle: Text(
+                              '重量: ${record.weight} kg, 回数: ${record.reps}回, 日時: $formattedDate'),
+                        ),
+                      );
+                    },
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('エラーが発生しました: ${snapshot.error}');
+                }
+                return CircularProgressIndicator();
+              },
+            ),
+          ),
+          SizedBox(height: 20),
+          Card(
+            margin: EdgeInsets.all(8),
+            child: ElevatedButton(
+              child: Text('トップ画面に戻る'),
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => AppScreen()),
                 );
               },
-            );
-          } else if (snapshot.hasError) {
-            return Text('エラーが発生しました: ${snapshot.error}');
-          }
-
-          return CircularProgressIndicator();
-        },
+            ),
+          ),
+        ],
       ),
     );
   }
